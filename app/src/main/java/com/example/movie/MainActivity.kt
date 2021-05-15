643baeef7cb51c6abf6418f52f8ec6fd1e5c5286
soo5717 <koo5717@naver.com>
2021-05-15 21:58:29 +0900

    chore: 프로젝트명 및 패키지명 변경
    kotlinproject =>  movie로 변경하였습니다.

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kotlinproject
+package com.example.movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/test/java/com/example/movie/ExampleUnitTest.kt
+++ app/src/test/java/com/example/movie/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.kotlinproject
+package com.example.movie
 
 import org.junit.Test
 
