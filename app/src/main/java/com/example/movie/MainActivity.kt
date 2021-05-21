d7e1b38222cbb756537b20022769a84cd63ad5bf
soo5717 <koo5717@naver.com>
2021-05-22 00:08:00 +0900

    feat: 좋아요/싫어요 버튼 클릭 시 동작 구현
    - 좋아요/싫어요 관련 XML id 변경
    - Selector를 사용하여 구현하였음

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.movie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.movie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initButton()
+    }
+
+
+    // 버튼 동작 초기화 메서드
+    private fun initButton() {
+        // 버튼 클릭 시 색상 바뀌고 숫자 올라가기
+        binding.buttonThumbUp.setOnClickListener {
+            if (it.isSelected) { // 취소했을 때
+                binding.textViewThumbUp.text = (binding.textViewThumbUp.text.toString().toInt() - 1).toString()
+            } else { // 선택했을 때
+                if(binding.buttonThumbDown.isSelected) { // 선택된 상태면
+                    binding.textViewThumbDown.text = (binding.textViewThumbDown.text.toString().toInt() - 1).toString()
+                    binding.buttonThumbDown.isSelected = !binding.buttonThumbDown.isSelected
+                }
+                binding.textViewThumbUp.text = (binding.textViewThumbUp.text.toString().toInt() + 1).toString()
+            }
+            it.isSelected = !it.isSelected
+        }
+        binding.buttonThumbDown.setOnClickListener {
+            if (it.isSelected) { // 취소했을 때
+                binding.textViewThumbDown.text = (binding.textViewThumbDown.text.toString().toInt() - 1).toString()
+            } else { // 선택 했을 때
+                if(binding.buttonThumbUp.isSelected) { // 선택된 상태면
+                    binding.textViewThumbUp.text = (binding.textViewThumbUp.text.toString().toInt() - 1).toString()
+                    binding.buttonThumbUp.isSelected = !binding.buttonThumbUp.isSelected
+                }
+                binding.textViewThumbDown.text = (binding.textViewThumbDown.text.toString().toInt() + 1).toString()
+            }
+            it.isSelected = !it.isSelected
+        }
     }
 }
