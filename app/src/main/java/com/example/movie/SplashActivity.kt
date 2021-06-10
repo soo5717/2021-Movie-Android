b4e68e878e8bde23ca253ad2daedcbffe06223d1
soo5717 <koo5717@naver.com>
2021-06-10 21:23:11 +0900

    feat: 스플래쉬 화면 구현
    - 핸들러로 3000 딜레이 설정

---
+++ app/src/main/java/com/example/movie/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.movie
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 파라미터 없는 Handler 는 Deprecated 됨.
+        // 다양한 방식 있으니까 찾아봐도 좋을 듯 함.
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MovieActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
