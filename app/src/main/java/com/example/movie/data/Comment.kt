f53a2bafda299b049b67b60d62ff0471519164ee
soo5717 <koo5717@naver.com>
2021-06-12 17:57:22 +0900

    style: 한줄평 데이터 클래스 줄바꿈

--- app/src/main/java/com/example/movie/data/Comment.kt
+++ app/src/main/java/com/example/movie/data/Comment.kt
@@ -1,4 +1,10 @@
 package com.example.movie.data
 
 // 한줄평 데이터 클래스
-data class Comment (val id: String, val time: String, val comment: String, val like: String, val rate: Float)
+data class Comment (
+    val id: String,
+    val time: String,
+    val comment: String,
+    val like: String,
+    val rate: Float
+)
